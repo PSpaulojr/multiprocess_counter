0b5f5bdc9a13ea590b48469e0a9e3dfe5b98f687
Pspaulojr <paulossergio.pjr@gmail.com>
2019-05-07 19:59:53 -0300

    Arrumei macros, constantes e adicionei comentarios pertinentes

--- src/main.c
+++ src/main.c
@@ -5,71 +5,81 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-unsigned int primo (unsigned int );
+#define NMAX         64
+#define N_PROCESSOS  3
+
+
+unsigned int verifica_primo (unsigned int N);
 
 int main() {  
+  unsigned int vetor_numeros[NMAX];
+  char c;                         /*auxilia na leitura da entrada*/
+  int qntd_numeros = 0;
 
-  unsigned int vetor_numeros[64];
-  char c;
-  int numeros= 0;
-
-  for(int b = 0; b < 64; b++){
+  for(int b = 0; b < NMAX; b++) {   /*forma de 'zerar' meu vetor*/
     vetor_numeros[b] = '\0';
   }
   
-  do{
-    c = scanf("%d", &vetor_numeros[numeros]);
-    numeros ++;
+  do {
+    c = scanf("%d", &vetor_numeros[qntd_numeros]);
+    qntd_numeros ++;
   } while( (c != EOF) && (c != '\n') );
 
-  pid_t filho[3];
+  pid_t filho[N_PROCESSOS];
 
   int protection = PROT_READ | PROT_WRITE;
   int visibility = MAP_SHARED | MAP_ANON;
   
-  int *j;
   int *qntd_primos;
 
   qntd_primos = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
-  j = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
 
   (*qntd_primos) = 0;
-  (*j) = 0;
 
-  for(int i= 0; i < 3 ; i++){
+  for(int i= 0; i < N_PROCESSOS ; i++){
     filho[i] = fork();
     if (filho[i]==0) {
-      for( int k = i ; k  < numeros; k += 3 ){
-        (*qntd_primos) += primo( vetor_numeros[k] );
+      
+      /* Processo filho(i) Opera */ 
+      
+      for( int k = i ; k  < qntd_numeros ; k += N_PROCESSOS ){
+        (*qntd_primos) += verifica_primo( vetor_numeros[k] );
       }
       exit(0);
     }
   }
 
-  for (int k = 0; k < 3; k++){
+  /* Processo Pai */
+  for (int k = 0; k < N_PROCESSOS; k++){
     waitpid (filho[k], NULL, 0);
   }
+
   printf("%d\n", *qntd_primos);
 
   return 0;
 }
 
 
-unsigned int primo (unsigned int N){
-  int div = 0;
+unsigned int verifica_primo (unsigned int N){
+  int divisores = 0;
+  
   for (unsigned int i = 1; i <= N; i++) {
-    if (div > 3){
+    if (divisores > N_PROCESSOS){
       break;
     }
+
     if (N % i == 0) { 
-     div++;
+     divisores++;
     }
   }
-  if (div == 2){
-    div = 1;
+
+  if (divisores == 2){
+    divisores = 1;
   }
-  else{
-    div = 0;
+
+  else {
+    divisores = 0;
   }
-  return div;
+
+  return divisores;
 }
